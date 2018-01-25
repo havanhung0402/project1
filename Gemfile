ad8d851a1ecb72e5fcdd38803f3e8adf735de6dd
havanhung <havanhung0402@gmail.com>
2018-01-25 14:05:31 +0700

    design layout

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'coffee-rails', '~> 4.2'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem "jquery-rails", "4.3.1"
+gem 'bootstrap-slider-rails'
 
 group :development, :test do
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-	
-  get 'static_pages/home'
 
-  get "/help", to: "static_pages#help"
-
+  get '/home' , to: 'static_pages#home'
+  get '/help', to: "static_pages#help"
   root 'static_pages#home'
 end
