f3e2a01b8598c2a7a25cf1fa2a7413169de45fb4
havanhung <havanhung0402@gmail.com>
2018-02-19 20:21:33 +0700

    add register manager

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,7 @@
 class Course < ApplicationRecord
 	
   belongs_to :user
+  has_many :registers
   validates :user_id, presence: true
   validates :content, presence: true
   validates :title, presence: true, length: {maximum: Settings.title_course_max_length}
