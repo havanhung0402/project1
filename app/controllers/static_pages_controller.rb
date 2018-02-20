c36624199b4bcbd1c42b38dcfe5688a2e37c567a
havanhung <havanhung0402@gmail.com>
2018-02-20 10:48:16 +0700

    show home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,9 @@
 class StaticPagesController < ApplicationController
 	
   def home
+  	@posts = Post.order(created_at: :desc).select(:id, :picture, :title, :content).first 4
+  	@courses = Course.order(created_at: :desc).select(:id, :title, :image,
+      :content).first 4
   end
 
   def help
