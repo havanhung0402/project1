6af56e485bfdb364af2b069b7f0afb22232c3866
havanhung <havanhung0402@gmail.com>
2018-02-21 14:38:57 +0700

    fix

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,8 @@
 class StaticPagesController < ApplicationController
-	
+  
   def home
-  	@posts = Post.order(created_at: :desc).select(:id, :picture, :title, :content).first 4
-  	@courses = Course.order(created_at: :desc).select(:id, :title, :image,
+    @posts = Post.order(created_at: :desc).select(:id, :picture, :title, :content).first 4
+    @courses = Course.order(created_at: :desc).select(:id, :title, :image,
       :content).first 4
   end
 
